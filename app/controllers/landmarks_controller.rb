70ceb44c680892c8b02b71cf01b5ad4c90b338ce
Tony <a-traina2018@nlaw.northwestern.edu>
2018-02-08 07:29:37 -0600

    final

---
+++ app/controllers/landmarks_controller.rb
@@ -0,0 +1,31 @@
+class LandmarksController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
